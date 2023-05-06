bliss-op5-t/device_oneplus_msm8998-common

msm8998-common: Import DeviceSettings from yaap
- Imported from
  https://github.com/yaap-op5t/yaap_device_oneplus_msm8998-common/commit/23bd1ed82d08941d840dede378fe2694ab0ded3b

Co-Authored-By: Ido Ben-Hur <idoybh2@gmail.com>
Co-Authored-By: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-Authored-By: Anirudh Gupta <anirudhgupta109@aosip.dev>
Co-Authored-By: David Sn <divad.nnamtdeis@gmail.com>
Co-Authored-By: NurKeinNeid <mralexman3000@gmail.com>
Co-Authored-By: Omkar Chandorkar <forumomkar@gmail.com>
Co-Authored-By: enesykaya <enesykaya@gmail.com>
Co-Authored-By: Adhitya Mohan <me@adhityamohan.in>
Co-Authored-By: Daniel Solomon <solomondaniel@google.com>
Co-Authored-By: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Co-Authored-By: LuK1337 <priv.luk@gmail.com>
Co-Authored-By: Luca Stefani <luca.stefani.ge1@gmail.com>
Co-Authored-By: Murtaza@Piplod <murtaza1926@gmail.com>
Co-Authored-By: Nauval Rizky <enuma.alrizky@gmail.com>
Co-Authored-By: Omkar Chandorkar <gotenksIN@aosip.dev>
Co-Authored-By: SKULSHADY <anushekprasal@gmail.com>
Co-Authored-By: ashwin4rc <ashwin2001achu@gmail.com>
Co-Authored-By: itsjoeoui <itsjoeoui@pixelexperience.org>
Co-Authored-By: maitreya29 <maitreyapatni30@gmail.com>
Co-Authored-By: maxwen <max.weninger@gmail.com>
Signed-off-by: Namkhai B <me@forkbomb9.ch>

DeviceSettings/src/com/yaap/device/DeviceSettings/DCModeTileService.java
@@ -1,0 +1,75 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.yaap.device.DeviceSettings;
+
+import android.content.SharedPreferences;
+import android.graphics.drawable.Icon;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import com.yaap.device.DeviceSettings.ModeSwitch.DCModeSwitch;
+
+public class DCModeTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        enabled = DCModeSwitch.isCurrentlyEnabled();
+        getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_dimming_on : R.drawable.ic_dimming_off));
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = DCModeSwitch.isCurrentlyEnabled();
+        Utils.writeValue(DCModeSwitch.getFile(), enabled ? "0" : "1");
+        sharedPrefs.edit().putBoolean(DeviceSettings.KEY_DC_SWITCH, !enabled).commit();
+        //getQsTile().setLabel(enabled ? "DC off" : "DC On");
+        getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_dimming_off : R.drawable.ic_dimming_on));
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceSettings/src/com/yaap/device/DeviceSettings/FPSTileService.java
@@ -1,0 +1,68 @@
+/**
+ * Copyright (C) 2020 YAAP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yaap.device.DeviceSettings;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import com.yaap.device.DeviceSettings.R;
+
+// TODO: Add FPS drawables
+public class FPSTileService extends TileService {
+
+  private final String KEY_FPS_INFO = "fps_info";
+
+  private boolean isShowing = false;
+
+  public FPSTileService() { }
+
+  @Override
+  public void onStartListening() {
+      super.onStartListening();
+      ActivityManager manager =
+              (ActivityManager) getSystemService(this.ACTIVITY_SERVICE);
+      for (ActivityManager.RunningServiceInfo service :
+              manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (com.yaap.device.DeviceSettings.FPSInfoService.class.getName().equals(
+                  service.service.getClassName())) {
+              isShowing = true;
+          }
+      }
+      updateTile();
+  }
+
+  @Override
+  public void onClick() {
+      Intent fpsinfo = new Intent(this, com.yaap.device.DeviceSettings.FPSInfoService.class);
+      if (!isShowing)
+          this.startService(fpsinfo);
+      else
+          this.stopService(fpsinfo);
+      isShowing = !isShowing;
+      updateTile();
+  }
+
+  private void updateTile() {
+      final Tile tile = getQsTile();
+      tile.setState(isShowing ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+      tile.updateTile();
+  }
+
+}

DeviceSettings/src/com/yaap/device/DeviceSettings/ModeSwitch/OnePlusModeSwitch.java
@@ -1,0 +1,50 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.yaap.device.DeviceSettings.ModeSwitch;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.yaap.device.DeviceSettings.Utils;
+
+public class OnePlusModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/virtual/graphics/fb0/oneplus_mode";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled() {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceSettings/src/com/yaap/device/DeviceSettings/PanelSettingsActivity.java
@@ -1,0 +1,33 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.yaap.device.DeviceSettings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PanelSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+                .add(android.R.id.content, new PanelSettings())
+                .commit();
+    }
+}

DeviceSettings/src/com/yaap/device/DeviceSettings/VibratorStrengthPreference.java
@@ -1,0 +1,105 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.yaap.device.DeviceSettings;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.ContentObserver;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Button;
+import android.os.Bundle;
+import android.util.Log;
+import android.os.Vibrator;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+
+public class VibratorStrengthPreference extends Preference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private SeekBar mSeekBar;
+    private int mOldStrength;
+    private int mMinValue;
+    private int mMaxValue;
+    private Vibrator mVibrator;
+
+    private static final String FILE_LEVEL = "/sys/class/leds/vibrator/vmax_mv";
+    private static final long testVibrationPattern[] = {0,250};
+
+    public VibratorStrengthPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mMinValue = 116;
+        mMaxValue = 1800;
+
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        setLayoutResource(R.layout.preference_seek_bar);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        mOldStrength = Integer.parseInt(getValue(getContext()));
+        mSeekBar = (SeekBar) holder.findViewById(R.id.seekbar);
+        mSeekBar.setMax(mMaxValue - mMinValue);
+        mSeekBar.setProgress(mOldStrength - mMinValue);
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(FILE_LEVEL);
+    }
+
+	public static String getValue(Context context) {
+		return Utils.getFileValue(FILE_LEVEL, "1800");
+	}
+
+	private void setValue(String newValue, boolean withFeedback) {
+	    Utils.writeValue(FILE_LEVEL, newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(DeviceSettings.KEY_VIBSTRENGTH, newValue);
+        editor.commit();
+        if (withFeedback) {
+            mVibrator.vibrate(testVibrationPattern, -1);
+        }
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported()) {
+            return;
+        }
+
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_VIBSTRENGTH, "1800");
+        Utils.writeValue(FILE_LEVEL, storedValue);
+    }
+
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+        // NA
+    }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // NA
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        setValue(String.valueOf(seekBar.getProgress() + mMinValue), true);
+    }
+}

REV3NT3CH-Devices/android_device_oneplus_msm8998-common

DeviceSettings/src/com/bliss/device/DeviceSettings/DCModeTileService.java
@@ -1,0 +1,75 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.bliss.device.DeviceSettings;
+
+import android.content.SharedPreferences;
+import android.graphics.drawable.Icon;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import com.bliss.device.DeviceSettings.ModeSwitch.DCModeSwitch;
+
+public class DCModeTileService extends TileService {
+    private boolean enabled = false;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        enabled = DCModeSwitch.isCurrentlyEnabled();
+        getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_dimming_on : R.drawable.ic_dimming_off));
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        enabled = DCModeSwitch.isCurrentlyEnabled();
+        Utils.writeValue(DCModeSwitch.getFile(), enabled ? "0" : "1");
+        sharedPrefs.edit().putBoolean(DeviceSettings.KEY_DC_SWITCH, !enabled).commit();
+        //getQsTile().setLabel(enabled ? "DC off" : "DC On");
+        getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_dimming_off : R.drawable.ic_dimming_on));
+        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        getQsTile().updateTile();
+    }
+}

DeviceSettings/src/com/bliss/device/DeviceSettings/FPSTileService.java
@@ -1,0 +1,68 @@
+/**
+ * Copyright (C) 2020 bliss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bliss.device.DeviceSettings;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import com.bliss.device.DeviceSettings.R;
+
+// TODO: Add FPS drawables
+public class FPSTileService extends TileService {
+
+  private final String KEY_FPS_INFO = "fps_info";
+
+  private boolean isShowing = false;
+
+  public FPSTileService() { }
+
+  @Override
+  public void onStartListening() {
+      super.onStartListening();
+      ActivityManager manager =
+              (ActivityManager) getSystemService(this.ACTIVITY_SERVICE);
+      for (ActivityManager.RunningServiceInfo service :
+              manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (com.bliss.device.DeviceSettings.FPSInfoService.class.getName().equals(
+                  service.service.getClassName())) {
+              isShowing = true;
+          }
+      }
+      updateTile();
+  }
+
+  @Override
+  public void onClick() {
+      Intent fpsinfo = new Intent(this, com.bliss.device.DeviceSettings.FPSInfoService.class);
+      if (!isShowing)
+          this.startService(fpsinfo);
+      else
+          this.stopService(fpsinfo);
+      isShowing = !isShowing;
+      updateTile();
+  }
+
+  private void updateTile() {
+      final Tile tile = getQsTile();
+      tile.setState(isShowing ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+      tile.updateTile();
+  }
+
+}

DeviceSettings/src/com/bliss/device/DeviceSettings/ModeSwitch/OnePlusModeSwitch.java
@@ -1,0 +1,50 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.bliss.device.DeviceSettings.ModeSwitch;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.bliss.device.DeviceSettings.Utils;
+
+public class OnePlusModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/virtual/graphics/fb0/oneplus_mode";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled() {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceSettings/src/com/bliss/device/DeviceSettings/PanelSettingsActivity.java
@@ -1,0 +1,33 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.bliss.device.DeviceSettings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PanelSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+                .add(android.R.id.content, new PanelSettings())
+                .commit();
+    }
+}

DeviceSettings/src/com/bliss/device/DeviceSettings/VibratorStrengthPreference.java
@@ -1,0 +1,105 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.bliss.device.DeviceSettings;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.ContentObserver;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Button;
+import android.os.Bundle;
+import android.util.Log;
+import android.os.Vibrator;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+
+public class VibratorStrengthPreference extends Preference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private SeekBar mSeekBar;
+    private int mOldStrength;
+    private int mMinValue;
+    private int mMaxValue;
+    private Vibrator mVibrator;
+
+    private static final String FILE_LEVEL = "/sys/class/leds/vibrator/vmax_mv";
+    private static final long testVibrationPattern[] = {0,250};
+
+    public VibratorStrengthPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mMinValue = 116;
+        mMaxValue = 1800;
+
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        setLayoutResource(R.layout.preference_seek_bar);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        mOldStrength = Integer.parseInt(getValue(getContext()));
+        mSeekBar = (SeekBar) holder.findViewById(R.id.seekbar);
+        mSeekBar.setMax(mMaxValue - mMinValue);
+        mSeekBar.setProgress(mOldStrength - mMinValue);
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(FILE_LEVEL);
+    }
+
+	public static String getValue(Context context) {
+		return Utils.getFileValue(FILE_LEVEL, "1800");
+	}
+
+	private void setValue(String newValue, boolean withFeedback) {
+	    Utils.writeValue(FILE_LEVEL, newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(DeviceSettings.KEY_VIBSTRENGTH, newValue);
+        editor.commit();
+        if (withFeedback) {
+            mVibrator.vibrate(testVibrationPattern, -1);
+        }
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported()) {
+            return;
+        }
+
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_VIBSTRENGTH, "1800");
+        Utils.writeValue(FILE_LEVEL, storedValue);
+    }
+
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+        // NA
+    }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // NA
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        setValue(String.valueOf(seekBar.getProgress() + mMinValue), true);
+    }
+}